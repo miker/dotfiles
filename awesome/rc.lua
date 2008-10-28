2e1e72551524c6a0287a1cdea55a42ac51ee3be4
Greg Fitzgerald <netzdamon@gmail.com>
2008-10-28 15:18:21 -0400

    Change os.date() format

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -492,7 +492,7 @@ function hook_timer ()
     -- For unix time_t lovers
     -- mytextbox.text = " " .. os.time() .. " time_t "
     -- Otherwise use:
-    mytextbox.text = " " .. os.date() .. " "
+    mytextbox.text = " " .. os.date("%A %B %d %I:%M:%S%P") .. " "
 end
 
 -- Set up some hooks
