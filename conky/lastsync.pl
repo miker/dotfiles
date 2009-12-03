25fe4e8132e93e8c1c93f0e0fa6ba917b0903954
Greg Fitzgerald <netzdamon@gmail.com>
2009-12-02 21:47:55 -0500

    remove old conky scrips and devilspie dir
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- conky/lastsync.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-use Date::Manip;
-
-$date = `grep "Sync completed" /var/log/emerge.log | tail -n1 | cut -c-10`;
-$date = &DateCalc("Jan 1, 1970  00:00:00 GMT",$date);
-$date = UnixDate("$date","%a %b %d %r");
-print "$date";
