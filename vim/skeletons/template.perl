478c61b17ff968e55c82dcba6b291315a6c548c1
Greg Fitzgerald <netzdamon@gmail.com>
2010-06-11 00:24:04 -0400

    Add some simple template files

---
+++ vim/skeletons/template.perl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl 
+# Distributed under the terms of the GNU General Public License v2
+# Copyright (C) %%YEAR%% %%AUTHOR%%
+# vim: set sw=2 sts=2 et tw=80 :
+
---
+++ vim/skeletons/template.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+# Distributed under the terms of the GNU General Public License v2
+# Copyright (C) %%YEAR%% %%AUTHOR%%
+# vim: set sw=4 sts=4 et tw=80 :
+
